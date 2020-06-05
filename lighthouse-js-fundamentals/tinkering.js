fd573b33eff3fca48ebb293e2cb05c490a7223a7
Mathius Johnson <devachievem@gmail.com>
2020-06-05 20:16:01 +0000

    Use conditionals to tell people about the weather

---
+++ lighthouse-js-fundamentals/tinkering.js
@@ -0,0 +1,13 @@
+function multiply(a, b) {
+  return a * b;
+}
+
+const result = multiply(2, 4);
+
+console.log(result);
+
+var my_string = "Mathius Johnson";
+
+var ASCII_value = my_string.charCodeAt(0);
+
+console.log(ASCII_value);
