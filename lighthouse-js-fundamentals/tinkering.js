ebf8a56d1a00738890c89c8bb4ea11488906dcb6
Mathius Johnson <devachievem@gmail.com>
2020-06-05 21:43:13 +0000

    first chorus commit

--- lighthouse-js-fundamentals/tinkering.js
+++ lighthouse-js-fundamentals/tinkering.js
@@ -11,3 +11,7 @@ var my_string = "Mathius Johnson";
 var ASCII_value = my_string.charCodeAt(0);
 
 console.log(ASCII_value);
+
+for (var i = 0; i < my_string.length; i++) {
+  console.log(my_string.charCodeAt)
+}
